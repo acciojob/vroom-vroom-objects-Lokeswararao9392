ba4edc899f3f4800ea5168e3275776dd750867d1
Lokeswararao9392 <lokeswararaomaddu4@gmail.com>
2025-10-22 00:36:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,36 @@
-// Complete the js code
-function Car(make, model) {}
+  function Car(make,model){
+       if(typeof make !== "string" || typeof model !== "string"){
+          throw new Error("Type of arguments should be strong")
+       }
+       this.make = make;
+       this.model = model;
+  }
 
-function SportsCar(make, model, topSpeed) {}
+  Car.prototype.getMakeModel = function(){
+    return (`${this.make} ${this.model}`)
+  }
 
-// Do not change the code below
-window.Car = Car;
-window.SportsCar = SportsCar;
+  function SportsCar(make,model,topSpeed){
+    if(typeof make !== "string" || typeof model !== "string" || typeof topSpeed !== "number"){
+        throw new Error("type of arguments not correct")
+    }
+    this.make = make;
+    this.model = model;
+    this.topSpeed = topSpeed;
+  }
+
+// Inheriting from Car
+   SportsCar.prototype = Car.prototype
+
+  SportsCar.prototype.getTopSpeed = function(){
+    return this.topSpeed
+  }
+
+  // Create a New SportsCar Instance
+   const car = new SportsCar ("Ferrari","Testarossa",200)
+
+   // Call getMakeModel() to get make model of the car
+   console.log(car.getMakeModel()) // Output :- Ferrari Testarossa
+
+   // call getTopSpeed() to get topSpeed of the SportCar
+   console.log(car.getTopSpeed()) // OutPut :- 200
